cb53e3204fcbd4c0c94ed414e216b58271e01a8b
Yacoubi2020 <73942925+Yacoubi2020@users.noreply.github.com>
2020-12-07 20:18:25 +0100

    Add files via upload

---
+++ graphique_0.java
@@ -0,0 +1,12 @@
+package swing;
+import javax.swing.* ;
+public class graphique_0 {
+
+	public static void main(String[] args) {
+	JFrame p=new JFrame("yyyy");
+	p.setTitle("Premier projet");
+	p.setSize(56,88);
+	p.setVisible(true);
+	}
+	}
+
---
+++ graphique_1.java
@@ -0,0 +1,18 @@
+package swing;
+import javax.swing.* ;
+public class graphique_1 {
+
+	public static void main(String[] args) {
+		JFrame u=new c();
+		
+	}
+
+}
+class c extends JFrame{
+	public c() {
+		setTitle("yyy");
+		setSize(29,89);
+		setVisible(true);
+		
+	}
+}
